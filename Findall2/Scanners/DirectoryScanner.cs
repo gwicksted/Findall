da2857c5af5ac63cfb237f42e0dc957b2557c6f9
Graeme Wicksted <user@server>
2013-02-28 20:17:43 -0500

    Added support for min and max file dates
    This functionality is largely untested.  Until it can be properly
    tested, consider this an unstable feature.

--- Findall2/Scanners/DirectoryScanner.cs
+++ Findall2/Scanners/DirectoryScanner.cs
@@ -20,6 +20,10 @@ namespace Findall2.Scanners
 
         private readonly bool _systemAllowed;
 
+        private readonly DateTime? _minDate;
+
+        private readonly DateTime? _maxDate;
+        
         /// <summary>
         /// Creates a new instance of DirectoryScanner.
         /// </summary>
@@ -36,6 +40,34 @@ namespace Findall2.Scanners
         /// If the <paramref name="path"/> could not be located on disk.
         /// </exception>
         public DirectoryScanner(string path, string pattern, bool recursive, bool hiddenAllowed, bool systemAllowed)
+            : this (path, pattern, recursive, hiddenAllowed, systemAllowed, null, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of DirectoryScanner.
+        /// </summary>
+        /// <param name="path">The full path to the base directory to start scanning from.</param>
+        /// <param name="pattern">The file name pattern for matching (such as *.*).</param>
+        /// <param name="recursive">Indicates subfolders should also be scanned.</param>
+        /// <param name="hiddenAllowed">Indicates hidden files are acceptable.</param>
+        /// <param name="systemAllowed">Indicates system files are acceptable.</param>
+        /// <param name="minimumFileDate">
+        /// The created or last write time of the file must be greater than or equal to this value.
+        /// Provide a null to indicate no constraint on minimum time.
+        /// </param>
+        /// <param name="maximumFileDate">
+        /// The created or last write time of the file must be less than or equal to this value.
+        /// Provide a null to indicate no constraint on the maximum time.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="path"/> is null or <see cref="string.Empty"/>.
+        /// If <paramref name="pattern"/> is null or <see cref="string.Empty"/>.
+        /// </exception>
+        /// <exception cref="DirectoryNotFoundException">
+        /// If the <paramref name="path"/> could not be located on disk.
+        /// </exception>
+        public DirectoryScanner(string path, string pattern, bool recursive, bool hiddenAllowed, bool systemAllowed, DateTime? minimumFileDate, DateTime? maximumFileDate)
         {
             if (string.IsNullOrEmpty(path))
             {
@@ -61,6 +93,10 @@ namespace Findall2.Scanners
             _hiddenAllowed = hiddenAllowed;
 
             _systemAllowed = systemAllowed;
+
+            _minDate = minimumFileDate;
+
+            _maxDate = maximumFileDate;
         }
 
         /// <summary>
@@ -73,8 +109,7 @@ namespace Findall2.Scanners
         }
 
         /// <summary>
-        /// Indicates the file attributes match what requirements were passed in during construction.
-        /// Examples are <see cref="FileAttributes.Hidden"/> or <see cref="FileAttributes.System"/>.
+        /// Indicates the file attributes and dates match what requirements were passed in during construction.
         /// </summary>
         /// <param name="file">The full path to the file on disk.</param>
         /// <returns>
@@ -83,12 +118,58 @@ namespace Findall2.Scanners
         /// </returns>
         private bool IsFileAcceptable(string file)
         {
-            FileAttributes attributes = new FileInfo(file).Attributes;
+            FileInfo info = new FileInfo(file);
+
+            return AreAttributesAcceptable(info) && AreTimesAcceptable(info);
+        }
+
+        /// <summary>
+        /// Indicates the file dates match what requirements were passed in during construction.
+        /// The <see cref="FileInfo.CreationTime"/> must be &gt;= the minimum date or the
+        /// <see cref="FileInfo.LastWriteTime"/> must be &gt;= the minimum date or the
+        /// minimum date passed in during custruction must be null.
+        /// The <see cref="FileInfo.CreationTime"/> must be &lt;= the maximum date or the
+        /// <see cref="FileInfo.LastWriteTime"/> must be &lt;= the maximum date or the
+        /// maximum date passed in during custruction must be null.
+        /// </summary>
+        /// <param name="info"><see cref="FileInfo"/> to collect file dates from.</param>
+        /// <returns>
+        /// True if the file matches the requirements.
+        /// False if it fails one or more requirement.
+        /// </returns>
+        private bool AreTimesAcceptable(FileInfo info)
+        {
+            bool valid = false;
+
+            if (_minDate != null || _maxDate != null)
+            {
+                DateTime created = info.CreationTime;
+                DateTime modified = info.LastWriteTime;
+
+                valid = (_minDate == null || created >= _minDate || modified >= _minDate) &&
+                        (_maxDate == null || created <= _maxDate || modified <= _maxDate);
+            }
+
+            return valid;
+        }
+
+        /// <summary>
+        /// Indicates the file attributes match what requirements were passed in during construction.
+        /// Examples are <see cref="FileAttributes.Hidden"/> or <see cref="FileAttributes.System"/>.
+        /// </summary>
+        /// <param name="info"><see cref="FileInfo"/> to collect attributes from.</param>
+        /// <returns>
+        /// True if the file matches the requirements.
+        /// False if it fails one or more requirement.
+        /// </returns>
+        private bool AreAttributesAcceptable(FileInfo info)
+        {
+            FileAttributes attributes = info.Attributes;
 
             bool fileIsHidden = (attributes & FileAttributes.Hidden) != FileAttributes.Hidden;
-            
+
             bool fileIsSystem = (attributes & FileAttributes.System) != FileAttributes.System;
-            
+
             return (_hiddenAllowed || fileIsHidden) && (_systemAllowed || fileIsSystem);
         }
     }
